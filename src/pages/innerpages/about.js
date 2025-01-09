904d9a978a169ef95a507730438458ef51333d3c
vinay <vinaykunduru@gmail.com>
2025-01-09 10:39:51 +0530

    home

--- src/pages/innerpages/about.js
+++ src/pages/innerpages/about.js
@@ -1,14 +1,14 @@
-import React from 'react';
-import Loader from '../../components/common/Loader';
-import Header from '../../components/innerpage/about/Header';
-import Navbar from '../../components/innerpage/Navbar';
-import About from '../../components/innerpage/about/About';
-import Process from '../../components/innerpage/about/Process';
-import Projects from '../../components/innerpage/about/Projects';
-import Awards from '../../components/innerpage/about/Awards';
-import Footer from '../../components/innerpage/Footer';
-import Menu from '../../components/innerpage/Menu';
-import { Helmet } from 'react-helmet';
+import React from "react";
+import Loader from "../../components/common/Loader";
+import Header from "../../components/innerpage/about/Header";
+import Navbar from "../../components/innerpage/Navbar";
+import About from "../../components/innerpage/about/About";
+import Process from "../../components/innerpage/about/Process";
+import Projects from "../../components/innerpage/about/Projects";
+import Awards from "../../components/innerpage/about/Awards";
+import Footer from "../../components/innerpage/Footer";
+import Menu from "../../components/innerpage/Menu";
+import { Helmet } from "react-helmet";
 
 function AboutPage() {
   return (
